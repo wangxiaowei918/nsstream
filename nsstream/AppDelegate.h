9156105e8f4e0246e0bb3887b0f1450393b681ed
wangxiaowei918 <wangxiaowei918@163.com>
2015-06-08 12:19:10 +0800

    Initial Commit

---
+++ nsstream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nsstream
+//
+//  Created by 王王 on 15/6/8.
+//  Copyright (c) 2015年 王晓伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nsstream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nsstream
+//
+//  Created by 王王 on 15/6/8.
+//  Copyright (c) 2015年 王晓伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
